97205881c1dfe59712a9aad2cf30aeeacd096d2a
Veerendranath <veerendranath0312@gmail.com>
2023-04-29 00:29:05 +0530

    Delete deployment files

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`)
 })
