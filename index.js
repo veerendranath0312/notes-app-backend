0e8a9993f30c7c24f9a69465e37cfbb379c66a3e
Veerendranath <veerendranath0312@gmail.com>
2023-04-28 23:01:45 +0530

    Add Fly.io deployment config

--- index.js
+++ index.js
@@ -1,76 +1,81 @@
-const express = require("express");
+const express = require('express')
+const cors = require('cors')
 
-const app = express();
-const PORT = 3001;
+const app = express()
 
-app.use(express.json()); // parse the incoming JSON
+app.use(cors())
+app.use(express.json()) // parse the incoming JSON
 
 let notes = [
   {
     id: 1,
-    content: "HTML is easy",
-    important: true,
+    content: 'HTML is easy',
+    important: true
   },
   {
     id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
+    content: 'Browser can execute only JavaScript',
+    important: false
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
-
-app.get("/", (req, res) => {
-  res.status(200).send("<h2>Welcome to Notes API</h2>");
-});
-
-app.get("/api/notes/:id", (req, res) => {
-  const { id } = req.params;
-  const note = notes.find((note) => note.id === Number(id));
-
-  if (!note) {
-    return res
-      .status(400)
-      .json({ msg: "Data not found or provide a valid number" });
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true
   }
+]
 
-  res.status(200).json({ data: note });
-});
+app.get('/', (req, res) => {
+  res.status(200).send('<h2>Welcome to Notes API</h2>')
+})
 
-app.delete("/api/notes/:id", (req, res) => {
-  const { id } = req.params;
+app.get('/api/notes', (req, res) => {
+  res.status(200).json(notes)
+})
 
-  notes = notes.filter((note) => note.id !== id);
+app.post('/api/notes', (req, res) => {
+  const data = req.body
 
-  res.status(204).end();
-});
-
-app.post("/api/notes", (req, res) => {
-  const data = req.body;
-
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0;
+  const maxId = notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0
 
   const newNote = {
     id: maxId + 1,
     content: data.content,
-    important: Math.random() < 0.5,
-  };
+    important: Math.random() < 0.5
+  }
 
-  notes = [...notes, newNote];
+  notes = [...notes, newNote]
 
-  res.status(201).json({ data: notes });
-});
+  res.status(201).json(newNote)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const { id } = req.params
+  const note = notes.find(note => note.id === Number(id))
+
+  if (!note) {
+    return res
+      .status(400)
+      .json({ msg: 'Data not found or provide a valid number' })
+  }
+
+  res.status(200).json({ note })
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params
+
+  notes = notes.filter(note => note.id !== id)
+
+  res.status(204).end()
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+  console.log(`Server running at http://localhost:${PORT}/`)
+})
