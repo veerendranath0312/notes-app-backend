34c3194cb385776ea51078bcb6ce661bf35f1500
Veerendranath <veerendranath0312@gmail.com>
2023-05-03 20:20:29 +0530

    Add mongoose validators

--- models/note.js
+++ models/note.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
   important: Boolean
 })
 
