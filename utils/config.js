0092bca77a4d4a4444b5b6a273949a2c25cca19d
Veerendranath <veerendranath0312@gmail.com>
2023-05-09 22:26:10 +0530

    Add tests related to notes API

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI
+
 const PORT = process.env.PORT | 8080
 
 module.exports = { MONGODB_URI, PORT }
