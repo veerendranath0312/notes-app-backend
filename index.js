a4fad2c0e1a6fcbf82bf4cc8ba404c5668fad502
Veerendranath <veerendranath0312@gmail.com>
2024-07-06 18:43:29 -0400

    Handle GET, POST, DELETE requests

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json()) // Parse the input data
 
-const notes = [
+let notes = [
   {
     id: 1,
     content: 'HTML is easy',
@@ -25,10 +26,53 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => {
+  res.status(200).json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id
+  const note = notes.find((note) => note.id === Number(id))
+
+  // Guard clause to check if the note with the requested is not found
+  if (!note) {
+    return res.status(404).json({ message: `Note with id ${id} is not found` })
+  }
+
+  res.json(note)
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (req, res) => {
+  const body = req.body
+
+  // Guard clause to check if the content is provided or not
+  if (!body.content) {
+    return res.status(400).json({ error: 'content missing' })
+  }
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    important: Boolean(body.important) || false,
+  }
+
+  notes = [...notes, note]
+
   res.json(notes)
 })
 
-const PORT = 3001
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id
+  notes = notes.filter((note) => note.id !== Number(id))
+
+  res.status(204).end()
+})
+
+const PORT = 8080
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
